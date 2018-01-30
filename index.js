a13f9d18dd4cc45e084bb7fc0fa96e4d2e697722
Shawn Cicoria <github@cicoria.com>
2018-01-30 16:19:41 +0200

    initial

---
+++ index.js
@@ -0,0 +1,6 @@
+import { AppRegistry } from 'react-native';
+import App from './App';
+import KeyStoreView from './components/KeyStoreView';
+
+AppRegistry.registerComponent('KeyStoreCheck', () => App);
+AppRegistry.registerComponent('KeyStoreView', () => KeyStoreView);
